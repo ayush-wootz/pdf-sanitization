06f852ece907b8568a9e33633622a1fee14fe6ed
Vishal-Wootz <vishaljaiswal@Vishals-MacBook-Air.local>
2026-01-21 15:40:28 +0530

    Created FileList.jsx to display uploaded files with remove functionality

---
+++ frontend/src/components/common/FileList.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { IconX } from '../assets/icons';
+
+/*
+FileList Component
+Displays a list of uploaded PDF files with remove buttons
+
+@param {Array} files - Array of File objects
+@param {Function} onRemove - Callback when remove button is clicked (index)
+*/
+
+export default function FileList({ files, onRemove }) {
+    if (!files || files.length === 0) {
+        return null;
+    }
+
+    return (
+        <div className="filelist">
+            <h3>Selected files</h3>
+            <ul className="files">
+                {files.map((file, index) => (
+                    <li key={`${file.name}-${index}`} className="file">
+                        <div style={{display: "flex", alignItems: "center", minWidth: 0}}>
+                            <span className="name">{file.name}</span>
+                            <span className="meta">{(file.size / 1024).toFixed(0)} KB</span>
+                        </div>
+                        <button
+                            type="button"
+                            className="btn remove"
+                            title="Remove"
+                            onClick={() => onRemove(index)}
+                        >
+                            <IconX /> Remove
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
